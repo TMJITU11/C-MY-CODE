ae44b9d2c6d18f1904497569d361842a6673d43b
JITU <taslimulmahbub255@gmail.com>
2025-09-05 22:44:27 +0600

    Add files via upload

---
+++ 1)Odd Even.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+
+    if (num > 0) {
+        cout << "The number is positive." << endl;
+
+        if (num % 2 == 0) {
+            cout << "The positive number is even." << endl;
+        } else {
+            cout << "The positive number is odd." << endl;
+        }
+    } else if (num < 0) {
+        cout << "The number is negative." << endl;
+    } else {
+        cout << "The number is zero." << endl;
+    }
+
+    return 0;
+}
+
