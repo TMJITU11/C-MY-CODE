91c78061766c35d78bad4e2753efdb074d2e6811
JITU <taslimulmahbub255@gmail.com>
2025-09-07 22:52:18 +0600

    Add files via upload

---
+++ 2)Vowel Consonent.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char character;
+
+    cout << "Enter a character: ";
+    cin >> character;
+
+    character = tolower(character);
+
+
+    if ((character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z')) {
+
+        if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u' ||
+            character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U') {
+            cout << "The character is a vowel." << endl;
+        } else {
+            cout << "The character is a consonant." << endl;
+        }
+    } else {
+        cout << "Not a character." << endl;
+    }
+
+    return 0;
+}
+
+
+
