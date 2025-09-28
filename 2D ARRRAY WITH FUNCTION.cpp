760c258595668105b7dcc4c4d7e08eff61236284
JITU <taslimulmahbub255@gmail.com>
2025-09-28 23:10:25 +0600

    Add files via upload

---
+++ 2D ARRRAY WITH FUNCTION.cpp
@@ -0,0 +1,39 @@
+//pass array to a function
+
+#include <iostream>
+using namespace std;
+
+void input(int arr[][3], int r, int c)
+ {
+    cout <<"enter the eliments of array :"<<endl;
+      for (int i=0; i<r; ++i) {
+          for (int j=0; j<c; ++j) {
+            cout << "enter element at position [" <<i<< "][" <<j<< "]: ";
+
+            cin >> arr[i][j];
+        }
+    }
+}
+
+
+void displayarray(int arr[][3], int r, int c)
+{
+    cout <<"the 2D array is:" <<endl;
+    for (int i=0; i<r; ++i) {
+        for (int j = 0; j<c; ++j) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    const int r = 3;
+    const int c = 3;
+    int arr[r][c];
+
+    input(arr, r,c);
+    displayarray(arr,r,c);
+
+    return 0;
+}
